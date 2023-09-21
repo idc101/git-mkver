b5b43b76ad245384095eb15cb32f8201283a412b
Iain Cardnell <idc101@users.noreply.github.com>
2023-09-21 20:49:20 +0100

    ci: upgrade graalvm

--- build-windows.ps1
+++ build-windows.ps1
@@ -9,4 +9,4 @@ Set-Location -Path target\scala-2.12
 & native-image -jar "git-mkver-assembly-$VERSION.jar" --no-fallback
 Move-Item -Path "git-mkver-assembly-$VERSION.exe" -Destination git-mkver.exe
 Compress-Archive -Path 'git-mkver.exe' -DestinationPath 'git-mkver-windows-amd64-%VERSION%.zip'
-Get-FileHash git-mkver-windows-amd64-%VERSION%.zip | %% Hash
+Get-FileHash git-mkver-windows-amd64-%VERSION%.zip | % Hash
